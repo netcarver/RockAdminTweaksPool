32aee49042675815e94136049d72defd8a33495e
Netcarver <id.github.com@gzap.org>
2024-05-11 19:28:49 +0100

    feat: Add AoS ModuleCollapseInfo

---
+++ assets/RockAdminTweaks/AdminOnSteroids/ModuleCollapseInfo.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace RockAdminTweaks;
+
+use ProcessWire\HookEvent;
+use ProcessWire\Inputfield;
+
+class ModuleCollapseInfo extends Tweak {
+
+
+    public function info()
+    {
+        return [
+            'description' => 'Collapse Module Info section by default',
+        ];
+    }
+
+
+    public function ready()
+    {
+        if ($this->wire->page->template != 'admin') {
+            return;
+        }
+
+        if ($this->page->process && $this->page->process == 'ProcessModule') {
+            $this->wire('processBrowserTitle', $this->input->get('name'));
+        }
+
+        $this->wire->addHookAfter('InputfieldMarkup::render', function (HookEvent $event) {
+            $field = $event->object;
+            if ($field->id === 'ModuleInfo') {
+                $field->collapsed = Inputfield::collapsedYes;
+            }
+        });
+    }
+}
